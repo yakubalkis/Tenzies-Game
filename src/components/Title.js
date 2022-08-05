f5fe02ca477984c4626e7a7d129317e834424d4a
yakubalkis <yakupalkiis@gmail.com>
2022-08-05 14:19:41 +0300

    Title completed.

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title(){
+    return (
+        <div className="header">
+            <h1>Tenzies</h1>
+            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+    )
+}
